6d6a9fc48adfa0eee31d0ac2ca5fbb96bdde8b0f
Manu Paliwal <paliwalmanu99@gmail.com>
2018-03-17 15:17:02 +0530

    Update moniter_Display.py

--- moniter_Display.py
+++ moniter_Display.py
@@ -4,7 +4,7 @@ from SocketServer import ThreadingMixIn
 import sqlite3
 from gmplot import gmplot
 import urllib2
-import webbrowser
+import webbrowser as wb
 import urllib3
 from selenium import webdriver
 import time
@@ -16,11 +16,28 @@ urllib2.install_opener(opener)
 l=[]
 global color
 global driver
+global gmap
 # Multithreaded Python server : TCP Server Socket Thread Pool
 
-class ClientThread(Thread): 
+
+
+gmap = gmplot.GoogleMapPlotter(18.554314, 73.7970365, 18.5)
+lcd_lat, lcd_lon = 18.554314, 73.7970365
+gmap.marker(lcd_lat, lcd_lon, 'black')
+gmap.draw("my_map.html")
+
+wb.open("file:///home/trinity/Desktop/HINT/my_map.html")
+
+refreshrate=3
+driver = webdriver.Firefox()
+driver.get("file:///home/trinity/Desktop/HINT/my_map.html")
+
+
+class ClientThread(Thread):
+  
     global driver
     global color
+    global gmap
     def __init__(self,ip,port): 
         Thread.__init__(self) 
         self.ip = ip 
@@ -29,37 +46,53 @@ class ClientThread(Thread):
  
     def run(self): 
         while True : 
+            data = ""
             data = conn.recv(2048) 
-            print "Server received data:", data
-	    lst=data.split()
-	    lat=lst[0];lon=lst[1];amt=lst[2];
-	    print lat+" "+lon+" "+amt
-            amt = int(amt)
-            if amt == 1:
-		color = 'yellow'
-            elif amt == 2:
-                color ='orange'
-            elif amt == 3:
-                color = 'red'
-            elif amt == 4:
-                color = 'red'
-            elif amt == 5:
-                color = 'red'
-                           
-	    l.append((float(lat),float(lon)))
-	    top_attraction_lats, top_attraction_lons = zip(*l)
-	    gmap.scatter(top_attraction_lats, top_attraction_lons, color)
-	    gmap.draw("my_map.html")
-            driver.refresh()	    
+            if(data <> ""):
+		    print "Server received data:", data
+		    lst=data.split()
+		    lat=lst[0];lon=lst[1];amt=lst[2];
+		    print lat+" "+lon+" "+amt
+		    amt = int(amt)
+		    if amt == 1:
+			color = 'yellow'
+		    elif amt == 2:
+		        color ='orange'
+		    elif amt == 3:
+		        color = 'red'
+		    elif amt == 4:
+		        color = 'red'
+		    elif amt == 5:
+		        color = 'red'
+		                   
+		    l.append((float(lat),float(lon)))
+		    top_attraction_lats, top_attraction_lons = zip(*l)
+		    gmap.scatter(top_attraction_lats, top_attraction_lons, color)
+		    
+		    gmap.draw("my_map.html")
+		    driver.refresh()	    
 	    
 
             #MESSAGE = raw_input("Multithreaded Python server : Enter Response from Server/Enter exit:")
             #if MESSAGE == 'exit':
             #   break
-            #conn.send(MESSAGE)  # echo 
 
-# Multithreaded Python server : TCP Server Socket Program Stub
+
 TCP_IP = '0.0.0.0' 
+TCP_PORT = 2005
+BUFFER_SIZE = 20  # Usually 1024, but we need quick response 
+
+tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+tcpServer.bind((TCP_IP, TCP_PORT)) 
+threads = [] 
+ 
+
+
+     
+'''
+Multithreaded Python server : TCP Server Socket Program Stub
+TCP_IP = '192.168.43.83' 
 TCP_PORT = 2005
 BUFFER_SIZE = 20  # Usually 1024, but we need quick response 
 
@@ -83,6 +116,8 @@ gmap.draw("my_map.html")
 driver = webdriver.Firefox()
 driver.get("file:///home/mahima/Desktop/HINT/my_map.html")
  
+'''
+
 while True: 
     tcpServer.listen(4) 
     print "Multithreaded Python server : Waiting for connections from TCP clients..." 
@@ -93,3 +128,4 @@ while True:
  
 for t in threads: 
     t.join() 
+
