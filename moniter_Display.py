2dc5d609c799dc9fd11cb1d98a80a31ed261f880
Manu Paliwal <paliwalmanu99@gmail.com>
2018-03-18 02:32:17 +0530

    Update moniter_Display.py

--- moniter_Display.py
+++ moniter_Display.py
@@ -8,19 +8,26 @@ import webbrowser as wb
 import urllib3
 from selenium import webdriver
 import time
+import os
+from shutil import move
+
 
 '''proxy = urllib2.ProxyHandler({'http':'172.31.100.14:3128', 'https':'172.31.100.14:3128'})
 opener = urllib2.build_opener(proxy)
 urllib2.install_opener(opener)
 '''
 l=[]
-global color
+
+color = 'red'
 global driver
 global gmap
+fl = 0
+
+
 # Multithreaded Python server : TCP Server Socket Thread Pool
 
 
-
+'''
 gmap = gmplot.GoogleMapPlotter(18.554314, 73.7970365, 18.5)
 lcd_lat, lcd_lon = 18.554314, 73.7970365
 gmap.marker(lcd_lat, lcd_lon, 'black')
@@ -29,8 +36,71 @@ gmap.draw("my_map.html")
 wb.open("file:///home/trinity/Desktop/HINT/my_map.html")
 
 refreshrate=3
+
 driver = webdriver.Firefox()
 driver.get("file:///home/trinity/Desktop/HINT/my_map.html")
+'''
+
+
+def lol(lat,lon):
+
+        print (lat,lon)
+        lat = round(lat,6)
+        lon = round(lon,6)
+        print "after rounding of"
+        print (lat,lon)
+	f1 = open("my_map1.html",'w')
+	f = open("my_map.html")
+	lines = f.readlines()
+	flag = 0
+
+	for l in lines:
+             print l
+	     l1 = "		var latlng = new google.maps.LatLng(" + str(lat) + ", "+str(lon) +");"
+	     
+	     if l.rstrip() == l1:
+		 flag = 1
+		 print "yes"
+	     
+	     if(flag == 1 and l.rstrip() == "		marker.setMap(map);") :
+		 
+		   f1.write("marker.setMap(null);\n")
+		   flag = 0
+		   print "heei"
+	     else :
+		   f1.write(l + "\n")
+
+	#os.remove("my_map.html")
+        #move("my_map.html","my_map1.html")
+        f1.close()
+        f.close()
+        wb.open("file:///home/trinity/Desktop/HINT/my_map1.html")
+
+
+def flag_set(l1, l2):
+    global fl
+    global gmap
+    global driver
+     
+    lcd_lat = float(l1)
+    lcd_lon = float(l2)
+    gmap = gmplot.GoogleMapPlotter(lcd_lat, lcd_lon, 11)
+		        	    
+    gmap.marker(lcd_lat, lcd_lon, 'black')
+    gmap.draw("my_map.html")
+
+    wb.open("file:///home/trinity/Desktop/HINT/my_map.html")
+    driver = webdriver.Firefox()
+    driver.get("file:///home/trinity/Desktop/HINT/my_map.html")
+    fl = 1
+
+
+def check():
+    global fl
+    if(fl == 0) :
+        return True
+    else:
+        return False
 
 
 class ClientThread(Thread):
@@ -38,36 +108,61 @@ class ClientThread(Thread):
     global driver
     global color
     global gmap
+    global l
+
     def __init__(self,ip,port): 
         Thread.__init__(self) 
         self.ip = ip 
         self.port = port 
         print "[+] New server socket thread started for " + ip + ":" + str(port) 
  
-    def run(self): 
+    def run(self):
+        #print flag 
         while True : 
             data = ""
             data = conn.recv(2048) 
             if(data <> ""):
 		    print "Server received data:", data
 		    lst=data.split()
-		    lat=lst[0];lon=lst[1];amt=lst[2];
-		    print lat+" "+lon+" "+amt
-		    amt = int(amt)
-		    if amt == 1:
-			color = 'yellow'
-		    elif amt == 2:
-		        color ='orange'
-		    elif amt == 3:
-		        color = 'red'
-		    elif amt == 4:
-		        color = 'red'
-		    elif amt == 5:
-		        color = 'red'
-		                   
-		    l.append((float(lat),float(lon)))
-		    top_attraction_lats, top_attraction_lons = zip(*l)
-		    gmap.scatter(top_attraction_lats, top_attraction_lons, color)
+                    if(len(lst) == 3):
+                          
+
+                            ind = (float(lst[0]), float(lst[1]))
+                            
+                                  
+                           
+                            #l.remove((float(lst[0]),float(lst[1]))) 
+                            lol(float(lst[0]),float(lst[1]))
+                      
+                            color='red' 
+                    else : 
+			    lcd_lat = lst[0];lcd_lon = lst[1]; lat=lst[2];lon=lst[3];amt=lst[4];
+                            l = []
+		            l.append((float(lat),float(lon)))
+		            if(check() == True):
+		                
+		                    flag_set(lcd_lat,lcd_lon)
+
+			    print lat+" "+lon+" "+amt
+			    amt = int(amt)
+			    if amt == 1:
+				color = 'yellow'
+			    elif amt == 2:
+				color ='orange'
+			    elif amt == 3:
+				color = 'red'
+			    elif amt == 4:
+				color = 'red'
+			    elif amt == 5:
+				color = 'red'
+				          
+			     
+		    
+                    
+			    user_lats, user_lons = zip(*l)
+			    print(user_lats)
+			    print(user_lons)
+			    gmap.scatter(user_lats, user_lons, color)
 		    
 		    gmap.draw("my_map.html")
 		    driver.refresh()	    
